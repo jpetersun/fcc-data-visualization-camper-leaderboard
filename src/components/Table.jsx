e2560dfacba19e6dbf98534596913402131d21e4
jpetersun <jpeterson903@yahoo.com>
2018-03-27 09:49:43 -0700

    feat: render sorted alltime and recent data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,24 +1,53 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import Row from './Row'
-import campers from '../data/alltime.json'
+
+import campersAllTime from '../data/alltime.json'
+import campersRecent from '../data/recent.json'
 
 class Table extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      campers
+      campers: [],
+      campersAllTime,
+      campersRecent
     }
+
+    this.handleClickRecent = this.handleClickRecent.bind(this)
+    this.handleClickAlltime = this.handleClickAlltime.bind(this)
   }
 
   componentDidMount () {
-    const campers = this.state.campers
-    // Least to most points
+    this.setState({campers: campersAllTime.alltime})
+  }
+
+  handleClickAlltime () {
+    const campers = Object.assign({}, this.state.campersAllTime)
+
+    // Most to least
+    // Data is already sorted, but can sort data otherwise
     const sorted = campers.alltime.sort((a, b) => {
-      return a.recent - b.recent
+      return b.alltime - a.alltime
     })
-    console.log(typeof campers)
-    console.log(typeof sorted)
+
+    const sortedCampers = sorted
+
+    this.setState({ campers: sortedCampers })
+  }
+
+  handleClickRecent () {
+    const campers = Object.assign({}, this.state.campersRecent)
+
+    // Most to least
+    // Data is already sorted, but can sort data otherwise
+    const sorted = campers.recent.sort((a, b) => {
+      return b.recent - a.recent
+    })
+
+    const sortedCampers = sorted
+
+    this.setState({ campers: sortedCampers })
   }
 
   render () {
@@ -28,12 +57,18 @@ class Table extends React.Component {
           <tr>
             <th className='camper-leaderboard__th'>#</th>
             <th className='camper-leaderboard__th'>Camper Name</th>
-            <th className='camper-leaderboard__th'>Points in past 30 days</th>
-            <th className='camper-leaderboard__th'>All time points</th>
+            <th onClick={this.handleClickRecent}
+              className='camper-leaderboard__th camper-leaderboard__th--sort'>
+              Points in past 30 days
+            </th>
+            <th onClick={this.handleClickAlltime}
+              className='camper-leaderboard__th camper-leaderboard__th--sort'>
+              All time points
+            </th>
           </tr>
         </thead>
         <tbody>
-          {this.state.campers.alltime.map((camper, idx) => {
+          {this.state.campers.map((camper, idx) => {
             return <Row key={camper.username} {...camper} idx={idx + 1} />
           })}
         </tbody>
